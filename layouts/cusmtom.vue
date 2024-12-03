e6df49b5bf3acb71768aed1d68de48f31046ef36
raul <74162376+raulanto@users.noreply.github.com>
2024-12-03 13:33:51 -0600

    cambios

---
+++ layouts/cusmtom.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+import {Slot} from "radix-vue";
+
+const components: { title: string, href: string, description: string }[] = [
+    {
+        title: 'Alert Dialog',
+        href: '/docs/components/alert-dialog',
+        description:
+            'A modal dialog that interrupts the user with important content and expects a response.',
+    },
+    {
+        title: 'Hover Card',
+        href: '/docs/components/hover-card',
+        description:
+            'For sighted users to preview content available behind a link.',
+    },
+    {
+        title: 'Progress',
+        href: '/docs/components/progress',
+        description:
+            'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+    },
+    {
+        title: 'Scroll-area',
+        href: '/docs/components/scroll-area',
+        description: 'Visually or semantically separates content.',
+    },
+    {
+        title: 'Tabs',
+        href: '/docs/components/tabs',
+        description:
+            'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
+    },
+    {
+        title: 'Tooltip',
+        href: '/docs/components/tooltip',
+        description:
+            'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+    },
+]
+</script>
+
+<template>
+    <section class="w-full flex justify-center items-center">
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul class="w-full justify-center\ grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
+                            <li class="row-span-3">
+                                <NavigationMenuLink as-child>
+                                    <a
+                                        class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                        href="/"
+                                    >
+                                        <img src="https://www.radix-vue.com/logo.svg" class="h-6 w-6">
+                                        <div class="mb-2 mt-4 text-lg font-medium">
+                                            Physical Change
+                                        </div>
+                                        <p class="text-sm leading-tight text-muted-foreground">
+                                            Nunca es Tarde para Conformas el cambio
+                                        </p>
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+
+                            <li>
+                                <NavigationMenuLink as-child>
+                                    <a
+                                        href="/docs/introduction"
+                                        class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                    >
+                                        <div class="text-sm font-medium leading-none">Introduction</div>
+                                        <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                            Re-usable components built using Radix UI and Tailwind CSS.
+                                        </p>
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+                            <li>
+                                <NavigationMenuLink as-child>
+                                    <a
+                                        href="/docs/installation"
+                                        class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                    >
+                                        <div class="text-sm font-medium leading-none">Installation</div>
+                                        <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                            How to install dependencies and structure your app.
+                                        </p>
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+                            <li>
+                                <NavigationMenuLink as-child>
+                                    <a
+                                        href="/docs/typography"
+                                        class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                    >
+                                        <div class="text-sm font-medium leading-none">Typography</div>
+                                        <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                            Styles for headings, paragraphs, lists...etc
+                                        </p>
+                                    </a>
+                                </NavigationMenuLink>
+                            </li>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/" :class="navigationMenuTriggerStyle()">
+                        Inicio
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink href="/autor/inicio/" :class="navigationMenuTriggerStyle()">
+                        Registro
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    </section>
+    <slot></slot>
+</template>
---
+++ pages/autor/inicio/index.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import core from "@redocly/ajv/lib/vocabularies/core";
+definePageMeta({
+    layout: 'cusmtom',
+
+})
+const orreo=ref()
+</script>
+
+<template>
+    <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+        <div class="flex items-center justify-center py-12">
+            <div class="mx-auto grid w-[350px] gap-6">
+                <div class="grid gap-2 text-center">
+                    <h1 class="text-2xl font-bold">
+                        Inicio de sesión
+                    </h1>
+                    <p class="text-balance text-muted-foreground">
+                        Ingrese su correo y contraseña
+                    </p>
+                </div>
+                <div class="grid gap-4">
+                    <div class="grid gap-2">
+                        <Label for="email">Correo</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="m@example.com"
+                            required
+                            v-model="orreo"
+                        />
+                    </div>
+                    <div class="grid gap-2">
+                        <div class="flex items-center">
+                            <Label for="password">contraseña</Label>
+
+                        </div>
+                        <Input id="password" type="password" required />
+                    </div>
+                    <Button type="submit" class="w-full">
+                        Inicio
+                    </Button>
+                    <Button variant="outline" class="w-full">
+                        Registro
+                    </Button>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="hidden bg-muted lg:block">
+            <img
+                src="~/assets/fondo12%201.svg"
+                alt="Image"
+                width="1920"
+                height="1080"
+                class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            >
+        </div>
+    </div>
+</template>
